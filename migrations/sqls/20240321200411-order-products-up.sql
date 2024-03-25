b0bb01b8aa33c6c658a5fe7b33e88304280e8d3b
Erny Sans <ernysans@icloud.com>
2024-03-25 11:44:00 -0500

    order_products migration: Check if the order status is open before inserting or updating an order product

--- migrations/sqls/20240321200411-order-products-up.sql
+++ migrations/sqls/20240321200411-order-products-up.sql
@@ -2,5 +2,23 @@ CREATE TABLE order_products (
   id SERIAL PRIMARY KEY,
   order_id INT REFERENCES orders(id) ON DELETE CASCADE,
   product_id INT REFERENCES products(id) ON DELETE CASCADE,
-  quantity int
+  quantity int NOT NULL
 );
+
+-- Check if the order status is open before inserting or updating an order product
+CREATE OR REPLACE FUNCTION check_order_status_open() RETURNS TRIGGER AS $$
+BEGIN
+  IF (SELECT status FROM orders WHERE id = NEW.order_id) != 'open' THEN
+    RAISE EXCEPTION 'Order status is not open';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER order_status_check_before_insert
+BEFORE INSERT ON order_products
+FOR EACH ROW EXECUTE PROCEDURE check_order_status_open();
+
+CREATE TRIGGER order_status_check_before_update
+BEFORE UPDATE ON order_products
+FOR EACH ROW EXECUTE PROCEDURE check_order_status_open();
